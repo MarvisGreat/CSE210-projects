992f454c1283375d4a99064964b2dd10e0f1870a
MarvisGreat <nmarvellous@byupathway.edu>
2025-11-06 16:43:38 +0100

    done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int numb = randomGenerator.Next(1, 100);
+
+        int value = -1;
+        int guesses = 0;
+
+        while (value != numb)
+        {    guesses++; 
+            Console.WriteLine("What is your guess? ");
+            string guess = Console.ReadLine();
+            value = int.Parse(guess);
+
+            string result = "";
+            if (value > numb)
+            {
+                result = "Lower";
+            }
+
+            else if (value < numb)
+            {
+                result = "Higher";
+            }
+            else
+            {
+                result = "You guessed it right!";
+            }
+
+            Console.WriteLine(result);
+            Console.WriteLine($"You made a total of {guesses} guesses.");
+        }
     }
 }
